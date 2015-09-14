bb963acb4bd7f7ee41b1a2861553d1dd7a7037b2
caleb <cburch@ikanow.com>
2015-09-14 18:35:39 -0400

    init testing push from intellij - maven

---
+++ SlackStandupBotJava/src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Caleb on 9/14/2015.
+ */
+public class App {
+    public static void main(String args[]) {
+        System.out.println("Starting up Slack StandupBot -- need to switch to log4j");
+    }
+}
--- src/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Caleb on 9/13/2015.
- */
-public class Test {
-    public static void main(String args[]) {
-        System.out.println("hi");
-    }
-}

f5919fde5a67ce770b3910ea14d974c0f4c9db74
caleb <cburch@ikanow.com>
2015-09-22 20:07:58 -0400

    init commit - switched to eclipse to get me going faster

---
+++ SlackStandupBotJava/src/main/java/com/beamersauce/slack_standupbot_java/bot/StandupBot.java
@@ -0,0 +1,7 @@
+package com.beamersauce.slack_standupbot_java.bot;
+
+/**
+ * Created by Caleb on 9/14/2015.
+ */
+public class StandupBot {
+}
---
+++ SlackStandupBotJava/src/main/java/com/beamersauce/slack_standupbot_java/commands/implementations/IStandupBotCommand.java
@@ -0,0 +1,7 @@
+package com.beamersauce.slack_standupbot_java.commands;
+
+/**
+ * Created by Caleb on 9/14/2015.
+ */
+public class IStandupBotCommand {
+}
